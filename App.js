d54ab070e8b1cdc02871df414b44c58bebf17458
christian marais <christimarais@hotmail.fr>
2023-01-26 20:29:41 +0400

    Installation de express,nodemon

--- App.js
+++ App.js
@@ -1,1 +1,5 @@
-console.log('hello node');
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/',(req,res) => res.send('Hello, Express!!!222'))
+app.listen(port,() =>console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
