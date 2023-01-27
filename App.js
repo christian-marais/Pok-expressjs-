913d8c98f4d350955a2fd129a3462aa72bee3674
christian marais <christimarais@hotmail.fr>
2023-01-27 07:54:44 +0400

    Création de l'api rest indiquant le nombre de pokemons et chaque pokemons

--- App.js
+++ App.js
@@ -1,5 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 3000
+let pokemons = require('./mock-pokemon.js')
 app.get('/',(req,res) => res.send('Hello, Express!!!222'))
+app.get('/api/pokemon/:id',(req,res)=>{
+    const id = parseInt(req.params.id)
+    const pokemon = pokemons.find(pokemon => pokemon.id === id)
+    res.send(`bonjour pokemons ${pokemon.name} :${id}`)
+})
+app.get('/api/pokemons', (req,res)=> {
+    res.send(` il y a ${pokemons.length} dans le pokedex`)
+})
 app.listen(port,() =>console.log(`Notre application Node est démarrée sur : http://localhost:${port}`))
