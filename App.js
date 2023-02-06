218f66b12d76742caa17f105d5213d2074acaf8d
christian marais <christimarais@hotmail.fr>
2023-02-06 10:25:59 +0400

    ajout du model pokemon et modification de la connexion à la bdd mysql en mariadb

--- App.js
+++ App.js
@@ -6,22 +6,43 @@ const bodyParser = require('body-parser')
 const {success, getUniqueId} = require('./helper.js')
 const morgan = require('morgan')
 const favicon = require('serve-favicon')
-const {Sequelize} = require('sequelize')
+const {Sequelize, DataTypes} = require('sequelize')
+let pokemons = require('./mock-pokemon.js')
+const PokemonModel = require('./src/models/pokemon.js')
 
 const sequelize = new Sequelize(
-    'pokedex',//nom de la bdd
+    'pokedex',
     'christian',
     '1997',
     {
         host:'localhost',
-        dialect :'mysql',
-        logging: false 
+        dialect :'mariadb',
+        dialectOptions :{
+            timezone:'Etc/GMT-2'
+        },
+        logging: false
     }
 )
- sequelize.authenticate()
+// const sequelize = new Sequelize(
+//     'pokedex',//nom de la bdd
+//     'christian',
+//     '1997',
+//     {
+//         host:'localhost',
+//         dialect :'mysql',
+//         logging: false
+//     }
+// )
+sequelize.authenticate()
     .then(() => console.log('la connexion à la bdd  a été établie'))
     .catch(error => console.error(`impossible de se connecter à la bdd ${error}`))
-app 
+
+// const Pokemon = PokemonModel(sequelize,DataTypes)
+
+sequelize.sync({force:true})
+.then(__=>console.log('la base de données "Pokedex" a bien été synchronisée '))
+
+    app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(morgan('dev'))
     .use(bodyParser.json())
@@ -45,7 +66,7 @@ app.get('/api/pokemon/:id',(req,res)=>{
     const pokemon = pokemons.find(pokemon => pokemon.id === id)
     //res.send(`bonjour pokemons ${pokemon.name} :${id}`)
     res.json(success(message,pokemon))
-    
+
 })
 app.get('/api/pokemons', (req,res)=> {
     // res.send(` il y a ${pokemons.length} dans le pokedex`)
@@ -54,7 +75,7 @@ app.get('/api/pokemons', (req,res)=> {
 })
 
 app.put('/api/pokemons/:id',(req,res)=>{
-    const id = parseInt(req.params.id) 
+    const id = parseInt(req.params.id)
     const pokemonUpdated = { ...req.body,id:id}
     pokemons = pokemons.map(pokemon =>{
         return pokemon.id === id ? pokemonUpdated:pokemon
@@ -68,7 +89,7 @@ app.delete('/api/pokemons/:id', (req,res) => {
     let pokemonDeleted = pokemons.find(pokemon => pokemon.id === id)
     pokemons = pokemons.filter(pokemon => pokemon.id !==id)
     const message = `le pokemon ${pokemonDeleted.name} a bien été supprimé`
-    
+
     // const message = `le pokemon ${pokemonDeleted} a bien été supprimé`
     res.json(success(message,pokemonDeleted))
 })
