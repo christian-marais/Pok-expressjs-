8cff1a2c0af3414427d6f4573aa8dbc17771d4b0
christian marais <christimarais@hotmail.fr>
2023-02-07 18:39:38 +0400

    factorisation de la gestion d'erreur 500 de deletePokemon

--- src/routes/deletePokemon.js
+++ src/routes/deletePokemon.js
@@ -2,19 +2,24 @@ const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
   app.delete('/api/pokemons/:id', (req, res) => {
-    Pokemon.findByPk(req.params.id).then(pokemon => {
+    Pokemon.findByPk(req.params.id)
+    .then(pokemon => {
+        if (pokemon === null){
+            const message = `le pokemon demandé n'existe pas. Réessayez avec un nouvel identifiant`
+            return res.status(404).json({message})
+        }
       const pokemonDeleted = pokemon;
-      Pokemon.destroy({
+      return Pokemon.destroy({
         where: { id: pokemon.id }
       })
       .then(_ => {
         const message = `Le pokémon avec l'identifiant n°${pokemonDeleted.id} a bien été supprimé.`
         res.json({message, data: pokemonDeleted })
       })
-      .catch(error =>{
-        const message = ` le pokemon n'a pas pu être ajouté. Réessayer dans quelques instants`
+    })
+    .catch(error =>{
+        const message = ` le pokemon n'a pas pu être supprimé. Réessayer dans quelques instants`
         res.status(500).json({message,data:error})
-      })
     })
   })
 }
