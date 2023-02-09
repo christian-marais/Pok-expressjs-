4dd2ba6e34b3331da8af5668e0725780740340bb
christian marais <christimarais@hotmail.fr>
2023-02-09 16:11:33 +0400

    modification de l'opérateur eq par l'opérateur like et ajour d'une limite 5 dans les resultats

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -10,7 +10,8 @@ module.exports = (app) => {
           name:{// 'name' est le nom de la propriété du modèle pokemon
             [Op.like]:`%${name}%` // 'name' est le critere de la recherche
           }
-        }
+        },
+        limit:5
       })
       .then(pokemons=>{
         const message = `Il y a ${pokemons.length} qui correspondent à la recherche ${name}`
