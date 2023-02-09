7755695e8b70033b54164340e0dab77afaf3e9e5
christian marais <christimarais@hotmail.fr>
2023-02-09 15:39:39 +0400

    ajout de la fonction de recherche par name dans le point de terminaison findAll

--- src/routes/findAllPokemons.js
+++ src/routes/findAllPokemons.js
@@ -2,7 +2,15 @@ const { Pokemon } = require('../db/sequelize')
   
 module.exports = (app) => {
   app.get('/api/pokemons', (req, res) => {
-    Pokemon.findAll()
+    if(req.query.name){
+      const name = req.query.name
+      return Pokemon.findAll({where:{name}})
+      .then(pokemons=>{
+        const message = `Il y a ${pokemons.length} qui correspondent à la recherche ${name}`
+        res.json({message,data:pokemons})
+      })
+    }else{
+      Pokemon.findAll()
       .then(pokemons => {
         const message = 'La liste des pokémons a bien été récupérée.'
         res.json({ message, data: pokemons })// méthode fournie par express
@@ -12,5 +20,6 @@ module.exports = (app) => {
         res.status(500).json({message,data:error})
         console.log(error)
       })
+    }
   })
 }
