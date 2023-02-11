65f3a0844fd93645cc796bf35c0123d0684948c6
christian marais <christimarais@hotmail.fr>
2023-02-11 15:43:40 +0400

    correction de la vérification du password avec bcrypt dans login

--- src/routes/login.js
+++ src/routes/login.js
@@ -15,7 +15,7 @@ module.exports = (app) => {
             return res.status(404).json({message})
         }
         bcrypt.compare(req.body.password, user.password).then(isPasswordValid => {
-            if(isPasswordValid) {
+            if(!isPasswordValid) {
                 const message = `Le mot de passe est incorrect`;
                 return res.status(401).json({ message})
             }
@@ -28,7 +28,7 @@ module.exports = (app) => {
         )
 
             const message = `L'utilisateur a été connecté avec succès`;
-            return res.json({ message, data: user,token })
+            return res.json({ message, data: user,token})
         })
     })
     .catch(error =>{
