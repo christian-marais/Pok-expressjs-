047a6bb741164e8569e7bded0d26ffe382dda743
christian marais <christimarais@hotmail.fr>
2023-02-12 10:19:46 +0400

    add database connection

--- src/db/sequelize.js
+++ src/db/sequelize.js
@@ -33,6 +33,33 @@ const Pokemon = PokemonModel(sequelize, DataTypes)
 const User = UserModel(sequelize, DataTypes)
   
 const initDb = () => {
+  return sequelize.sync().then(_ => {
+    console.log("INIT DB")
+    pokemons.map(pokemon => {
+      Pokemon.create({
+        name: pokemon.name,
+        hp: pokemon.hp,
+        cp: pokemon.cp,
+        picture: pokemon.picture,
+        types: pokemon.types
+      }).then(pokemon => console.log(pokemon.toJSON()))
+    })
+
+    bcrypt.hash('1997', 10).then(hash=>{
+      User.create({
+        username: "christian",
+        email: "christimarais@hotmail.fr",
+        password:hash
+      }).then(user => console.log(user.toJSON()))
+  
+    })
+   
+    console.log('La base de donnée a bien été initialisée !')
+  })
+}
+
+/*
+const initDb = () => {
   return sequelize.sync({force: true}).then(_ => {
     console.log("INIT DB")
     pokemons.map(pokemon => {
@@ -57,7 +84,7 @@ const initDb = () => {
     console.log('La base de donnée a bien été initialisée !')
   })
 }
-  
+   */
 module.exports = { 
   initDb, Pokemon, User
 }
